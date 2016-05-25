983e6c9d2fee447846257635a7a8fdc3878bbe5f
Joshua Yuen <jyuen@yahoo-inc.com>
2016-05-25 13:37:35 -0700

    add user for dashboard

--- index.js
+++ index.js
@@ -32,8 +32,12 @@ var dashboard = new ParseDashboard({
     appId: process.env.APP_ID || 'myAppId',
     masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
     appName: "MyApp"
+  }],
+  users: [{
+    "user":"user",
+    "pass":"pass"
   }]
-});
+}, true);
 
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
